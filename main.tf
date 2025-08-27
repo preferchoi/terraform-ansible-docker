e99e9166d0484a175a90ab15bc46707d1d9c1818
preferchoi <prefer_choi@naver.com>
2025-08-27 20:21:23 +0900

    refactoring: formating

--- main.tf
+++ main.tf
@@ -3,13 +3,17 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = { Name = "terra-vpc" }
+  tags = {
+    Name = "terra-vpc"
+  }
 }
 
 # 게이트웨이 선언
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags   = { Name = "terra-igw" }
+  tags = {
+    Name = "terra-igw"
+  }
 }
 
 resource "aws_subnet" "public_a" {
@@ -17,15 +21,19 @@ resource "aws_subnet" "public_a" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.service_zone}a"
   map_public_ip_on_launch = true
-  tags                    = { Name = "terra-public-subnet-a" }
+  tags = {
+    Name = "terra-public-subnet-a"
+  }
 }
 
 resource "aws_subnet" "public_c" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "${var.service_zone}c"
   map_public_ip_on_launch = true
-  tags                    = { Name = "terra-public-subnet-c" }
+  tags = {
+    Name = "terra-public-subnet-c"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
@@ -39,12 +47,12 @@ resource "aws_route_table" "public_rt" {
 
 resource "aws_route_table_association" "public_a" {
   route_table_id = aws_route_table.public_rt.id
-  subnet_id = aws_subnet.public_a.id
+  subnet_id      = aws_subnet.public_a.id
 }
 
 resource "aws_route_table_association" "public_c" {
   route_table_id = aws_route_table.public_rt.id
-  subnet_id = aws_subnet.public_c.id
+  subnet_id      = aws_subnet.public_c.id
 }
 
 
