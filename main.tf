664d8ca5766e3ece900b1de2a4d78d8e70b47d37
preferchoi <prefer_choi@naver.com>
2025-08-27 20:05:38 +0900

    main.tf: add subnet a/c

--- main.tf
+++ main.tf
@@ -9,6 +9,21 @@ resource "aws_vpc" "main" {
 # 게이트웨이 선언
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = {Name="terra-igw"}
+  tags   = { Name = "terra-igw" }
 }
 
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.service_zone}a"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "terra-public-subnet-a" }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.service_zone}c"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "terra-public-subnet-c" }
+}
