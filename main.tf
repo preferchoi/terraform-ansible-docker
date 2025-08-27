4358c1309375ec517d16c36c74d215f14100873f
preferchoi <prefer_choi@naver.com>
2025-08-27 20:00:33 +0900

    main.tf: add vpc

---
+++ main.tf
@@ -0,0 +1,8 @@
+# vpc 선언
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = { Name = "terra-vpc" }
+}
+
