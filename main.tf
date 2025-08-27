7f85be55be9ea0468c30a5a65fc424eccd0f0035
preferchoi <prefer_choi@naver.com>
2025-08-27 20:29:49 +0900

    main.tf: add aws_security_group web/alb

--- main.tf
+++ main.tf
@@ -55,5 +55,55 @@ resource "aws_route_table_association" "public_c" {
   subnet_id      = aws_subnet.public_c.id
 }
 
+resource "aws_security_group" "alb_sg" {
+  name   = "terra-alb-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "terra-alb-sg" }
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "terra-web-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "terra-web-sg" }
+
+}
 
 
