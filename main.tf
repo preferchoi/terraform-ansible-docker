47439f377edad6c18aa371f08fdeab5fd5904ddd
preferchoi <prefer_choi@naver.com>
2025-08-27 20:20:55 +0900

    main.tf: add aws_route_table

--- main.tf
+++ main.tf
@@ -27,3 +27,25 @@ resource "aws_subnet" "public_c" {
   map_public_ip_on_launch = true
   tags                    = { Name = "terra-public-subnet-c" }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "terra-public-rt" }
+}
+
+resource "aws_route_table_association" "public_a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id = aws_subnet.public_a.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id = aws_subnet.public_c.id
+}
+
+
+
